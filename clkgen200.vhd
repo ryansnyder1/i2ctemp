a7b343caf3c90db9a529b46addfee663d6cd5f5b
rjs2995 <rjs2995@rit.edu>
2024-03-05 12:05:32 -0500

    initial files

---
+++ clkgen200.vhd
@@ -0,0 +1,35 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity clkgen200 is 
+port(
+    clk_100mhz : in std_logic;
+    clk_200khz : out std_logic 
+    );
+
+architecture behav of clkgen200 is
+
+--100Mhz/200khz = 500 -> 500/2 (half the period) = 250 -> 8 bit representation
+signal scount : std_logic_vector(7 downto 0) := "00000000";
+signal sclk200 : std_logic := '1';
+
+begin
+    process(clk_100mhz)
+    begin
+        if rising_edge(clk_100mhz) then
+                if scount = "11111001" then --249
+                    scount <= "00000000";
+                    sclk200 <= not sclk200;
+                else
+                    scount <= std_logic_vector(unsigned(scount) + 1);
+                end if;
+        end if;
+    end process;
+
+    clk_200khz <= sclk200;
+
+
+end architecture behav;
+
+
---
+++ top.vhd
@@ -0,0 +1,65 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity top is 
+    port(
+        CLK_100MHZ : in std_logic;
+        rst : in std_logic;
+        tmp_sda : inout std_logic;
+        tmp_scl : out std_logic;
+        leds : out std_logic_vector(7 downto 0)
+        --seg : out std_logic_vector(6 downto 0);
+        --anodes : out std_logic_vector(3 downto 0);
+        --nanodes : out std_logic_vector(3 downto 0);
+    );
+end top;
+
+architecture behav of top
+    ----component declarations----
+    component clkgen200 is
+        port(
+        clk_100mhz : in std_logic;
+        clk_200khz : out std_logic 
+        );
+
+    component i2c_master is
+        port(
+            clk_200khz : in std_logic;
+            rst : in std_logic;
+            sda : inout std_logic;
+            tmp : out std_logic_vector(7 downto 0);
+            sda_dir : out std_logic;
+            scl : out std_logic
+        );
+    end component i2c_master;
+
+    --7 segment display component declaration goes here
+
+    ----signal declarations----
+    signal ssda_dir: std_logic;
+    signal sclk200 : std_logic; -- 200 kHz SCL clock signal
+    signal stmp : std_logic_vector(7 downto 0); -- 8-bit temperature data result
+
+begin
+    clkgen200_inst : clkgen200 port map (
+        clk_100mhz => CLK_100MHZ,
+        clk_200khz => sclk200
+    );
+
+    i2cmaster_inst : i2c_master port map (
+        clk_200khz => sclk200,
+        rst => rst,
+        sda => tmp_sda,
+        tmp => stmp,
+        sda_dir => ssda_dir,
+        scl => tmp_scl;
+    );
+
+    --7 segment display component instantiation goes here
+
+    LED <= stmp;  --map 8-bit temperature data to on-board LEDs
+
+end behav;
+
+
